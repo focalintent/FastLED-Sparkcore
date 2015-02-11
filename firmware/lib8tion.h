2a6ef6e68f5a4ec88d250274abe08ec04fec209d
Daniel Garcia <danielgarcia@gmail.com>
2015-02-10 23:20:32 -0500

    Forward declaration of millis doesn't play nicely on spark core

--- firmware/lib8tion.h
+++ firmware/lib8tion.h
@@ -1868,7 +1868,7 @@ typedef q<uint16_t, 12,4> q124;
 // by #defining USE_GET_MILLISECOND_TIMER.
 #if (defined(ARDUINO) || defined(SPARK)) && !defined(USE_GET_MILLISECOND_TIMER)
 // Forward declaration of Arduino function 'millis'.
-uint32_t millis();
+// uint32_t millis();
 #define GET_MILLIS millis
 #else
 uint32_t get_millisecond_timer();
